d2e1d5c88a5e29b68307157bb201f80fba4b70c5
dapengchina <aoqishequ@163.com>
2018-01-12 18:30:29 +0800

    更改

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+ entry: './src/app.jsx',
+ output: {
+     path: __dirname + '/dist',
+     filename: "bundle.js"
+ },
+ module: {
+    loaders: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+    }],
+ },
+ plugins: [
+    new HtmlwebpackPlugin({
+        template: './public/app.html', //指定模板路径
+        filename: 'app.html', //指定文件名
+    }),
+    new OpenBrowserPlugin({
+        url: 'http://localhost:8080'
+    })
+ ]
+};
